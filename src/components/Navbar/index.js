c6b6a22e9b27a1e8f819def0ec42b00ddddb2e67
Talita Beneves <talitabeneves15@gmail.com>
2021-11-03 03:05:30 -0300

    Styling Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { 
+  Nav, 
+  NavbarContainer,
+  NavLogo, 
+  MobileIcon,
+  NavMenu, 
+  NavItem, 
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+
+} from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+      <>
+          <Nav>
+            <NavbarContainer>
+              <NavLogo to='/'> V </NavLogo>
+              <MobileIcon>
+               <FaBars />
+              </MobileIcon>
+              <NavMenu>
+                <NavItem>
+                  <NavLinks to="about">About</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="discover">Discover</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="services">Services</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="signup">Signup Up</NavLinks>
+                </NavItem>
+              </NavMenu>
+              <NavBtn>
+                <NavBtnLink to='/signup'>Signup In</NavBtnLink>
+              </NavBtn>
+            </NavbarContainer>
+          </Nav>
+      </>
+    );
+}
+
+export default Navbar
