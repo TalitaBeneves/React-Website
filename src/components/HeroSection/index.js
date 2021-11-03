eb80cae088e44254fde8a20f2fb9e819286e963a
Talita Beneves <talitabeneves15@gmail.com>
2021-11-03 20:16:39 -0300

    update

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElements';
 import { 
     HeroContainer,
     HeroBg, 
@@ -8,7 +9,6 @@ import {
     HeroH1,
     HeroP,
     HeroBtnWrapper,
-    Button,
     ArrowForward,
     ArrowRight
 } from './HeroElements';
@@ -31,7 +31,13 @@ const HeroSection = () => {
                     Astra, a Agente ganense, utiliza energias cósmicas para moldar o campo de batalha a seu bel-prazer. Com total domínio da sua forma astral e um talento estratégico nato, ela está sempre anos-luz à frente dos inimigos.
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button 
+                        to="signup" 
+                        onMouseEnter={onHover} 
+                        onMouseLeave={onHover}
+                        primary='true'
+                        dark='true'
+                    >
                         Get started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
