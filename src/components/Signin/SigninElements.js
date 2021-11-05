01ced0ec9010a13278fbd5c1e4782655725bcba2
Talita Beneves <talitabeneves15@gmail.com>
2021-11-05 07:14:13 -0300

    Creating Sign In Component

---
+++ src/components/Signin/SigninElements.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  min-height: 692px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 0;
+  overflow: hidden;
+  background: #191d33;
+`;
+
+export const FormWrap = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 400px){
+    height: 80%;
+  }
+`;
+
+export const Icon = styled(Link)`
+  margin-left: 32px;
+  margin-top: 32px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
+  font-size: 32px;
+  
+  &:hover{
+      transition: all 0.3s ease-in-out;
+      color: #ff4854;
+  } 
+
+  @media screen and (max-width: 480px){
+    margin-left: 16px;
+    margin-top: 8px
+  }
+`;
+
+export const FormContent = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 480px){
+    padding: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  background: #3d497d;
+  max-width: 400px;
+  height: auto;
+  width: 100%;
+  z-index: 1;
+  display: grid;
+  margin: 0 auto;
+  padding: 80px 32px;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
+
+  @media screen and (max-width: 400px){
+    padding: 32px 32px;
+  }
+`;
+
+export const FormH1 = styled.h1`
+  margin-bottom: 40px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 400px;
+  text-align: center;
+`;
+
+export const FormLabel = styled.label`
+  margin-bottom: 8px;
+  font-size: 14px;
+  color: #fff;
+`;
+
+export const FormInput = styled.input`
+  padding: 16px 16px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+`;
+
+export const FormButton = styled.button`
+  background: #532a55;
+  padding: 16px 0;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover{
+    transition: all 0.3s ease-in-out;
+    background: #3a2b4d;
+  } 
+`;
+
+export const Text = styled.span`
+  text-align: center;
+  margin-top: 24px;
+  color: #fff;
+  font-size: 14px;
+`;
---
+++ src/components/Signin/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Form, FormH1, FormContent, FormInput, FormLabel, FormWrap, Icon, FormButton, Text } from './SigninElements'
+
+const SignIn = () => {
+  return (
+    <>
+      <Container>
+        <FormWrap>
+          <Icon to="/">Agentes V</Icon>
+          <FormContent>
+            <Form action="#">
+              <FormH1>Faça seu Login</FormH1>
+              <FormLabel htmlFor='for'>Email</FormLabel>
+              <FormInput type='email' required />
+              <FormLabel htmlFor='for'>Password</FormLabel>
+              <FormInput type='password' required />
+              <FormButton type='submit'>Continue</FormButton>
+              <Text></Text>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+    </>
+  )
+}
+
+export default SignIn
