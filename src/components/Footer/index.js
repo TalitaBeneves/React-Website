5f4b611db99c75d27842c16ef995843bcdfecb5b
Talita Beneves <talitabeneves15@gmail.com>
2021-11-05 02:27:06 -0300

    Styling Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { 
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer, 
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink, 
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+
+
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Inventors</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contat Us</FooterLinkTitle>
+              <FooterLink to="/">Contact</FooterLink>
+              <FooterLink to="/">Support</FooterLink>
+              <FooterLink to="/">Destinations</FooterLink>
+              <FooterLink to="/">Sponsoships</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/">Submit Videos</FooterLink>
+              <FooterLink to="/">Ambassadors</FooterLink>
+              <FooterLink to="/">Agency</FooterLink>
+              <FooterLink to="/">Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="https://www.instagram.com/talita_beneves/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">Linkedin</FooterLink>
+              <FooterLink to="/">Rocket</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>
+              Agentes V
+            </SocialLogo>
+            <WebsiteRights>Talita Beneves © {new Date().getFullYear()} 
+              <br/> All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="https://www.instagram.com/talita_beneves/">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="https://www.facebook.com/talita.beneves">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="https://github.com/TalitaBeneves">
+                <FaGithub />
+              </SocialIconLink>
+              <SocialIconLink href="https://linkedin.com/in/talitabeneves/">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  )
+}
+
+export default Footer
