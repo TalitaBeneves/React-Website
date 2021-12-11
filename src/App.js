bfa95a2b2e89ea7146ac0f1958d9cb0c0dff136a
Talita Beneves <talitabeneves15@gmail.com>
2021-12-11 19:44:25 -0300

    site publicado no gh-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import  Home  from './pages';
 import  SigninPage  from './pages/signin';
 
 
 function App() {
   return (
-    <Router>
+    <>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={SigninPage} />
       </Switch>
-    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
